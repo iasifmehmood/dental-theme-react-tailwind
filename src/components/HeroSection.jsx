e42850d45c38b68abec043bf923ddc4002dab3f5
Asif Mehmood <asif-mehmood@hotmail.com>
2023-09-12 06:04:20 +0500

    fixed hero text-div bug

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <section
       name='home'
-      className='relative w-full bg-cover bg-left md:bg-center bg-no-repeat bg-center md:bg-left bg-fixed bg-gray-800 md:py-20'
+      className='relative w-full bg-cover bg-left md:bg-center bg-no-repeat md:py-20'
       id='hero'
     >
       <video
@@ -13,20 +13,27 @@ const HeroSection = () => {
         loop
         muted
         playsInline
-        className='absolute top-0 left-0 w-full h-full object-cover filter brightness-75'
+        className='absolute top-0 left-0 w-full h-full object-cover'
+        style={{
+          filter: 'brightness(75%)',
+        }}
       >
         <source src='/dentist2.mp4' type='video/mp4' />
       </video>
-
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center min-h-screen'>
-        <div className='md:w-1/2 text-center md:text-left relative z-10 mx-auto md:mx-0'>
-          <h2 className='font-semibold text-2xl sm:text-3xl md:text-5xl leading-tight md:leading-10 text-white uppercase drop-shadow-lg'>
-            Let us brighten your smile
+      <div className='max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8 flex items-center min-h-screen'>
+        <div className='md:w-1/2 text-center md:text-left relative z-9 mx-auto md:mx-0'>
+          <h2
+            className='font-semibold text-3xl md:text-5xl leading-10 text-white uppercase drop-shadow-lg'
+            // style={{
+            //   textShadow: "1px 1px 2px pink",
+            // }}
+          >
+            let us brighten your smile
           </h2>
-          <p className='py-5 leading-7 text-white drop-shadow-lg'>
+          <p className='py-5 leading-8 text-white drop-shadow-lg'>
             Creating healthier smiles through expert care and precision.
           </p>
-          <Button text='Make Appointment' className='relative z-10' />
+          <Button text='Make Appointment' className='relative z-9' />
         </div>
       </div>
     </section>
@@ -34,9 +41,6 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
-
-// import React from 'react';
-// import Button from './Button';
 
 // const HeroSection = () => {
 //   return (
