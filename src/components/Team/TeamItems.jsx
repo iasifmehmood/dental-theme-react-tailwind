f1f5c7675cdbb43ea89a1f80e501424a2fc29b1a
Asif Mehmood <asif-mehmood@hotmail.com>
2023-09-13 08:36:49 +0500

    added doc name

--- src/components/Team/TeamItems.jsx
+++ src/components/Team/TeamItems.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import './TeamItems.css';
 
@@ -17,6 +17,25 @@ import slide_image_5 from '../../assets/pic-2.png';
 import slide_image_6 from '../../assets/pic-3.png';
 
 function TeamItems() {
+  const doctorNames = [
+    'Dr. Smith',
+    'Dr. Johnson',
+    'Dr. Williams',
+    'Dr. Jones',
+    'Dr. Sarah',
+    'Dr. Ally',
+    // Add more doctor names as needed
+  ];
+
+  const [currentDoctorName, setCurrentDoctorName] = useState(doctorNames[0]);
+
+  const handleSwiper = swiper => {
+    swiper.on('slideChange', () => {
+      const activeIndex = swiper.realIndex;
+      setCurrentDoctorName(doctorNames[activeIndex]);
+    });
+  };
+
   return (
     <div className='container'>
       <h1 className='heading'>Our Team</h1>
@@ -32,13 +51,13 @@ function TeamItems() {
           depth: 100,
           modifier: 2.5,
         }}
-        // pagination={{ el: '.swiper-pagination', clickable: true }}
+        onSwiper={handleSwiper} // Call this function when Swiper is initialized
         navigation={{
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
           clickable: true,
         }}
-        mousewheel={true} // Enable mouse slide navigation
+        mousewheel={true}
         modules={[EffectCoverflow, Pagination, Navigation]}
         className='swiper_container'
       >
@@ -64,12 +83,87 @@ function TeamItems() {
         <div className='swiper-pagination'></div>
         <div className='swiper-button-next'></div>
         <div className='swiper-button-prev'></div>
+
+        <div className='doctor-name-container'>{currentDoctorName}</div>
       </Swiper>
     </div>
   );
 }
 
 export default TeamItems;
+
+// import React from 'react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import './TeamItems.css';
+
+// import 'swiper/css';
+// import 'swiper/css/effect-coverflow';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
+
+// import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+
+// import slide_image_1 from '../../assets/pic-4.jpg';
+// import slide_image_2 from '../../assets/pic-5.jpg';
+// import slide_image_3 from '../../assets/pic-6.jpg';
+// import slide_image_4 from '../../assets/pic-1.png';
+// import slide_image_5 from '../../assets/pic-2.png';
+// import slide_image_6 from '../../assets/pic-3.png';
+
+// function TeamItems() {
+//   return (
+//     <div className='container'>
+//       <h1 className='heading'>Our Team</h1>
+//       <Swiper
+//         effect={'coverflow'}
+//         grabCursor={true}
+//         centeredSlides={true}
+//         loop={true}
+//         slidesPerView={'auto'}
+//         coverflowEffect={{
+//           rotate: 0,
+//           stretch: 0,
+//           depth: 100,
+//           modifier: 2.5,
+//         }}
+//         // pagination={{ el: '.swiper-pagination', clickable: true }}
+//         navigation={{
+//           nextEl: '.swiper-button-next',
+//           prevEl: '.swiper-button-prev',
+//           clickable: true,
+//         }}
+//         mousewheel={true} // Enable mouse slide navigation
+//         modules={[EffectCoverflow, Pagination, Navigation]}
+//         className='swiper_container'
+//       >
+//         <SwiperSlide>
+//           <img src={slide_image_1} alt='slide_image' />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <img src={slide_image_2} alt='slide_image' />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <img src={slide_image_3} alt='slide_image' />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <img src={slide_image_4} alt='slide_image' />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <img src={slide_image_5} alt='slide_image' />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <img src={slide_image_6} alt='slide_image' />
+//         </SwiperSlide>
+
+//         <div className='swiper-pagination'></div>
+//         <div className='swiper-button-next'></div>
+//         <div className='swiper-button-prev'></div>
+//       </Swiper>
+//     </div>
+//   );
+// }
+
+// export default TeamItems;
 
 // import 'swiper/css';
 // import 'swiper/css/autoplay';
